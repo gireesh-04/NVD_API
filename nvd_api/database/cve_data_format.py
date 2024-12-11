3c4c75412f05a2429474f8975fd2f8711459a80b
gireesh-04 <gireeshbollineni@gmail.com>
2024-12-11 22:10:11 +0530

    Initial Commit

--- nvd_api/database/cve_data_format.py
+++ nvd_api/database/cve_data_format.py
@@ -1,10 +1,8 @@
 from mongoengine import Document, StringField, BooleanField, EmbeddedDocument, EmbeddedDocumentField, ListField, FloatField
 from mongoengine import connect
 
-# Connect to MongoDB
 connect('NVD_Data', host='mongodb+srv://gireesh_04:ROWifs5BzMANczn4@cluster0.rfcdd.mongodb.net/NVD_Data')
 
-# Embedded schema for CVSS metrics
 class CvssData(EmbeddedDocument):
     version = StringField(required=True)
     vectorString = StringField(required=True)
@@ -29,36 +27,28 @@ class CvssMetricV2(EmbeddedDocument):
     obtainOtherPrivilege = BooleanField(required=True, default=False)
     userInteractionRequired = BooleanField(required=True, default=False)
 
-# Embedded schema for descriptions
 class Description(EmbeddedDocument):
     lang = StringField()
     value = StringField()
 
-# Embedded schema for configuration nodes
-
-# Define the Node class as an EmbeddedDocument
 class CpeMatch(EmbeddedDocument):
     vulnerable = BooleanField(default=False)
     criteria = StringField()
     matchCriteriaId = StringField()
 
-# Define the Node class as an EmbeddedDocument
 class Node(EmbeddedDocument):
     operator = StringField()
     negate = BooleanField(default=False)
-    cpeMatch = ListField(EmbeddedDocumentField(CpeMatch))  # List of CpeMatch embedded documents
-
-# Define Configuration class as an EmbeddedDocument
+    cpeMatch = ListField(EmbeddedDocumentField(CpeMatch))  
 class Configuration(EmbeddedDocument):
-    nodes = ListField(EmbeddedDocumentField(Node))  # List of Embedded Node documents
+    nodes = ListField(EmbeddedDocumentField(Node)) 
 
-# Define CVE class as a Document, which includes configurations as an EmbeddedDocument
 class CVE(Document):
     cve_id = StringField()
     sourceIdentifier = StringField()
     published = StringField()
     lastModified = StringField()
     vulnStatus = StringField()
-    descriptions = ListField()  # Assuming Description is an embedded document
-    metrics = ListField()  # Assuming CVSS data is stored here
+    descriptions = ListField()  
+    metrics = ListField()  
     configurations = ListField(EmbeddedDocumentField(Configuration))
